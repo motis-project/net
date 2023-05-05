d02b8fd728e7e4650631acfd7f6dc05aa0ebfee6
Pablo Hoch <pablo@melaxis.com>
2023-05-05 15:55:19 +0200

    web server: add gzip support

---
+++ src/web_server/content_encoding.cc
@@ -0,0 +1,77 @@
+#include "net/web_server/content_encoding.h"
+
+#include <cstdlib>
+
+#include "boost/beast/http/rfc7230.hpp"
+
+#include "boost/iostreams/device/back_inserter.hpp"
+#include "boost/iostreams/filter/gzip.hpp"
+#include "boost/iostreams/filtering_stream.hpp"
+
+namespace bio = boost::iostreams;
+namespace http = boost::beast::http;
+
+namespace net {
+
+http_content_encoding select_content_encoding(
+    boost::beast::string_view const accept_encoding) {
+  auto const is_acceptable = [](http::param_list const& params) {
+    for (auto const& param : params) {
+      if (param.first == "q" && !param.second.empty()) {
+        std::string q = param.second;
+        auto const value = std::atof(q.data());
+        if (value == 0.0) {
+          return false;
+        }
+      }
+    }
+    return true;
+  };
+  auto any_acceptable = false;
+  for (auto const& ext : http::ext_list{accept_encoding}) {
+    if (ext.first == "gzip") {
+      return is_acceptable(ext.second) ? http_content_encoding::GZIP
+                                       : http_content_encoding::IDENTITY;
+    } else if (ext.first == "*") {
+      any_acceptable = is_acceptable(ext.second);
+    }
+  }
+  return any_acceptable ? http_content_encoding::GZIP
+                        : http_content_encoding::IDENTITY;
+}
+
+std::string gzip_content(
+    std::string_view const content,
+    bio::gzip_params const& gzip_params = bio::gzip::default_compression) {
+  auto compressed = std::string{};
+  auto sink = bio::back_inserter(compressed);
+  auto stream = bio::filtering_ostream{};
+  stream.push(bio::gzip_compressor{gzip_params});
+  stream.push(sink);
+  stream << content;
+  stream.pop();
+  return compressed;
+}
+
+void set_response_body(web_server::string_res_t& res,
+                       http_content_encoding const encoding,
+                       std::string_view const content) {
+  switch (encoding) {
+    case http_content_encoding::IDENTITY:
+      res.body() = std::string{content};
+      break;
+    case http_content_encoding::GZIP:
+      res.set(http::field::content_encoding, "gzip");
+      res.body() = gzip_content(content);
+      break;
+  }
+}
+
+void set_response_body(web_server::string_res_t& res,
+                       web_server::http_req_t const& req,
+                       std::string_view const content) {
+  set_response_body(
+      res, select_content_encoding(req[http::field::accept_encoding]), content);
+}
+
+}  // namespace net
